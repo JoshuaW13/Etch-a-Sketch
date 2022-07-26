9ab40dc66ab006e94d469ab97931c2d334be4897
Josh <josh.walters@shaw.ca>
2022-07-26 11:22:39 -0600

    added drwaing in black

--- sketch.js
+++ sketch.js
@@ -4,8 +4,15 @@ function setGrid(gridDimensions){
     for(let i =0;i<gridDimensions*gridDimensions;i++){
         let newCell = document.createElement("div");
         newCell.classList.add("gridItem");
+        newCell.addEventListener('mousedown', changeCellColorFirst)
+        newCell.addEventListener('mouseup', stopDrag)
+        newCell.addEventListener('mouseenter', changeCellColor)
+        
+        
         grid.appendChild(newCell);
     }
+
+    
     
     return
 
@@ -17,8 +24,46 @@ function changeGridSize(e){
     else if(this.classList.contains('32x32')){setGrid(32);}
 }
 
+function stopDrag(e){
+    mouseHeld = false;
+}
+
+function changeCellColor(e){
+    if(mouseHeld == true){
+        this.style.border = "none";
+        if(color == 1){this.style.background = `black`;}
+
+    }
+    
+}
+
+function changeCellColorFirst(e){
+    mouseHeld = true;
+    this.style.border = "none";
+    if(color == 1){this.style.background = `black`;}
+}
+
+//Event listeners to change grid sizes
 let sizes = document.querySelectorAll(".gridSize");
 sizes.forEach(size => size.addEventListener('click', changeGridSize));
+
+
+
+//Setup default grid
+let mouseHeld = false;
 let gridSize = 8;
+let color = 1;
 let grid = document.getElementById('grid');
 setGrid(gridSize);
+
+/* Color key:
+black: 1
+white: 2
+red: 3
+orange: 4
+yellow: 5
+Green: 6
+Blue: 7
+Indigo: 8
+Violet: 9
+*/
