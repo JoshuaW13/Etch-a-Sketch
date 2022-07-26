d9a4b214214956a86025423ea4b25afb7c598049
Josh <josh.walters@shaw.ca>
2022-07-26 10:40:54 -0600

    dynamic grid sizes added

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 function setGrid(gridDimensions){
+    grid.innerHTML = "";
     grid.style.gridTemplateColumns = `repeat(${gridDimensions}, 1fr)`;
     for(let i =0;i<gridDimensions*gridDimensions;i++){
         let newCell = document.createElement("div");
@@ -10,6 +11,14 @@ function setGrid(gridDimensions){
 
 }
 
+function changeGridSize(e){
+    if(this.classList.contains('8x8')){setGrid(8);}
+    else if(this.classList.contains('16x16')){setGrid(16);}
+    else if(this.classList.contains('32x32')){setGrid(32);}
+}
+
+let sizes = document.querySelectorAll(".gridSize");
+sizes.forEach(size => size.addEventListener('click', changeGridSize));
 let gridSize = 8;
 let grid = document.getElementById('grid');
 setGrid(gridSize);
