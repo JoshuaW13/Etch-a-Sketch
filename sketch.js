734ac734d03a1ca00866a02c91e43ed64e381847
Josh <josh.walters@shaw.ca>
2022-07-31 21:38:20 -0600

    Added ability to use different colors

--- sketch.js
+++ sketch.js
@@ -28,8 +28,25 @@ function setGrid(gridDimensions){
 function changeColor(e){
     console.log(e.type);
     if(mouseHeld || e.type === 'mousedown'){
-        e.target.style.background = 'black'
         e.target.style.border = 'none'
+        if(color === 1){e.target.style.background = 'black';}
+        else if(color === 2){e.target.style.background = 'white';}
+        else if(color === 3){e.target.style.background = 'red';}
+        else if(color === 4){e.target.style.background = 'orange';}
+        else if(color === 5){e.target.style.background = 'yellow';}
+        else if(color === 6){e.target.style.background = 'green';}
+        else if(color === 7){e.target.style.background = 'blue';}
+        else if(color === 8){e.target.style.background = 'indigo';}
+        else if(color === 9){e.target.style.background = 'violet';}
+        else if(color === 10){e.target.style.background = 'brown';}
+        else if(color === 11){e.target.style.background = 'grey';}
+        else if(color === 12){
+            e.target.style.background = 'white';
+            e.target.style.border = 'solid #eae8e4'
+            e.target.style.borderWidth = '1px'
+        }
+        
+        
     }
 
 }
@@ -39,6 +56,22 @@ function changeGridSize(e){
     else if(this.classList.contains('16x16')){setGrid(16);}
     else if(this.classList.contains('32x32')){setGrid(32);}
 }
+
+function changeBrush(e){
+    if(this.classList.contains('black')){color = 1;}
+    else if(this.classList.contains('white')){color = 2;}
+    else if(this.classList.contains('red')){color = 3;}
+    else if(this.classList.contains('orange')){color = 4;}
+    else if(this.classList.contains('yellow')){color = 5;}
+    else if(this.classList.contains('green')){color = 6;}
+    else if(this.classList.contains('blue')){color = 7;}
+    else if(this.classList.contains('indigo')){color = 8;}
+    else if(this.classList.contains('violet')){color = 9;}
+    else if(this.classList.contains('brown')){color = 10;}
+    else if(this.classList.contains('grey')){color = 11;}
+    else if(this.classList.contains('eraser')){color = 12;}   
+}
+
 
 
 
@@ -54,6 +87,8 @@ let gridSize = 8;
 let color = 1;
 let grid = document.getElementById('grid');
 setGrid(gridSize);
+let colors = document.querySelectorAll('.color');
+colors.forEach(color => color.addEventListener('click', changeBrush))
 
 /* Color key:
 black: 1
